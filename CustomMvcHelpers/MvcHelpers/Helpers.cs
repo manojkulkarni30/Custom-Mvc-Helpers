0e0a9b3a4292fda1d3eea8dcb2848acbc521f594
manojkulkarni30 <manojkulkarni30@gmail.com>
2015-06-16 21:08:01 +0530

    Initial Checkin

---
+++ CustomMvcHelpers/MvcHelpers/Helpers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CustomMvcHelpers.MvcHelpers
+{
+    public static class Helpers
+    {
+        /// <summary>
+        ///     Create Lable with Required Hint
+        /// </summary>
+        /// <typeparam name="TModel">Model</typeparam>
+        /// <typeparam name="TValue">Value</typeparam>
+        /// <param name="html">Helper method attach to HtmlHelper class</param>
+        /// <param name="expression">Linq Expression</param>
+        /// <param name="htmlAttributes">Html Attributes</param>
+        /// <returns></returns>
+        public static MvcHtmlString LabelRequiredFor<TModel, TValue>(
+            this HtmlHelper<TModel> html,
+            Expression<Func<TModel, TValue>> expression,
+            object htmlAttributes = null)
+        {
+            // Returns the metadata for the given expression from the model
+            var metadata = ModelMetadata.FromLambdaExpression(expression, html.ViewData);
+
+            //Gets the model name from an expression.
+            string htmlFieldName = ExpressionHelper.GetExpressionText(expression);
+            string labelText = metadata.DisplayName ?? metadata.PropertyName ?? htmlFieldName.Split('.').Last();
+            if (String.IsNullOrEmpty(labelText))
+            {
+                return MvcHtmlString.Empty;
+            }
+
+            var tag = new TagBuilder("label");
+            tag.MergeAttributes(new RouteValueDictionary(htmlAttributes));
+            tag.Attributes.Add("for", html.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldId(htmlFieldName));
+            tag.SetInnerText(labelText);
+
+            if (metadata.IsRequired)
+            {
+                var sup = new TagBuilder("sup");
+                sup.SetInnerText("*");
+                tag.InnerHtml += sup;
+            }
+
+            return MvcHtmlString.Create(tag.ToString(TagRenderMode.Normal));
+        }
+    }
+}
